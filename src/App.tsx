06a0bc095ec9acede265f1d40430065d24943acc
jesusbenagal <jesusguerreropaniagua6@gmail.com>
2023-06-11 20:24:25 +0200

    Started Virtualization

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { columns } from "./constants/column";
 import { makeData } from "./helpers/makeData";
 
 function App() {
+  const data = makeData(10000);
+
   return (
     <div>
-      <Datagrid columnsTable={columns} dataTable={makeData(100)} />
+      <Datagrid columnsTable={columns} dataTable={data} />
     </div>
   );
 }
--- src/components/Datagrid.tsx
+++ src/components/Datagrid.tsx
@@ -1,6 +1,5 @@
-import { useMemo } from "react";
+import { useMemo, useRef, useState } from "react";
 import {
-  TableContainer,
   Table,
   TableHead,
   TableBody,
@@ -12,9 +11,13 @@ import {
   flexRender,
   getCoreRowModel,
   useReactTable,
+  Row,
+  SortingState,
+  getSortedRowModel,
 } from "@tanstack/react-table";
 
 import StyledWrapper from "./StyleWrapper";
+import { useVirtual } from "react-virtual";
 
 interface Props {
   columnsTable: any[];
@@ -25,28 +28,74 @@ export const Datagrid = ({ columnsTable, dataTable }: Props) => {
   const columns = useMemo(() => columnsTable, [columnsTable]);
   const data = useMemo(() => dataTable, [dataTable]);
 
+  const [sorting, setSorting] = useState<SortingState>([]);
+
   const table = useReactTable({
     columns,
     data,
+    state: {
+      sorting,
+    },
+    onSortingChange: setSorting,
     getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    debugTable: true,
   });
 
+  const tableContainerRef = useRef<HTMLDivElement>(null);
+
+  const { rows } = table.getRowModel();
+
+  const rowVirtualizer = useVirtual({
+    parentRef: tableContainerRef,
+    size: rows.length,
+    overscan: 10,
+  });
+
+  const { virtualItems: virtualRows, totalSize } = rowVirtualizer;
+
+  const paddingTop = virtualRows.length > 0 ? virtualRows?.[0]?.start || 0 : 0;
+  const paddingBottom =
+    virtualRows.length > 0
+      ? totalSize - (virtualRows?.[virtualRows.length - 1]?.end || 0)
+      : 0;
+
   return (
-    <div
-      style={{
-        position: "relative",
-      }}
-    >
+    <div ref={tableContainerRef}>
       <StyledWrapper>
         <Table className="table">
           <TableHead className="thead">
             {table.getHeaderGroups().map((headerGroup) => (
               <TableRow className="tr" key={headerGroup.id}>
                 {headerGroup.headers.map((header) => (
-                  <TableCell className="th" key={header.id}>
-                    {flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
+                  <TableCell
+                    className="th"
+                    key={header.id}
+                    colSpan={header.colSpan}
+                    style={{
+                      width: header.getSize(),
+                    }}
+                  >
+                    {header.isPlaceholder ? null : (
+                      <div
+                        {...{
+                          onClick: header.column.getToggleSortingHandler(),
+                          style: {
+                            cursor: header.column.getCanSort()
+                              ? "pointer"
+                              : "default",
+                          },
+                        }}
+                      >
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                        {{
+                          asc: " 🔼",
+                          desc: " 🔽",
+                        }[header.column.getIsSorted() as string] ?? null}
+                      </div>
                     )}
                   </TableCell>
                 ))}
@@ -54,15 +103,38 @@ export const Datagrid = ({ columnsTable, dataTable }: Props) => {
             ))}
           </TableHead>
           <TableBody className="tbody">
-            {table.getRowModel().rows.map((row) => (
-              <TableRow className="tr" key={row.id}>
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell className="td" key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
+            {paddingTop > 0 && (
+              <TableRow>
+                <TableCell
+                  className="td"
+                  style={{
+                    height: `${paddingTop}px`,
+                  }}
+                />
               </TableRow>
-            ))}
+            )}
+            {virtualRows.map((virtualRow) => {
+              const row = rows[virtualRow.index];
+              return (
+                <TableRow className="tr" key={row.id}>
+                  {row.getVisibleCells().map((cell) => {
+                    return (
+                      <TableCell className="td" key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+            {paddingBottom > 0 && (
+              <TableRow>
+                <TableCell style={{ height: `${paddingBottom}px` }} />
+              </TableRow>
+            )}
           </TableBody>
         </Table>
       </StyledWrapper>
